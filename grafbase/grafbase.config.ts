04fe83f42100908143df2837d011482c58ade1d5
Maihar <maihar7051@gmail.com>
2023-10-27 15:45:00 +0530

    first commit

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,26 +1,27 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, auth, config } from '@grafbase/sdk';
 
-const User = g.model('User',{
-  name:g.string().length({min:2,max:20}),
-  email:g.string().unique(),
-  avatarUrl:g.url(),
-  descrition:g.string().optional(),
-  githubUrl:g.url().optional(),
+const User = g.model('User', {
+  name: g.string().length({ min: 2, max: 20 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().optional(),
+  githubUrl: g.url().optional(),
   linkedInUrl: g.url().optional(),
-  projects:g.relation(() => Project).list().optional(),
-})
-const Project = g.model('Project',{
-  title:g.string().length({min:3}),
-  description:g.string(),
-  image:g.url(),
-  liveSiteUrl:g.url(),
-  githubUrl:g.url().optional(),
-  category:g.string().search(),
-  createdBy:g.relation(() => User),
+  projects: g.relation(() => Project).list().optional(),
+});
 
-})
+const Project = g.model('Project', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(),
+  image: g.url(),
+  liveSiteUrl: g.url(),
+  githubUrl: g.url().optional(),
+  category: g.string().search(),
+  createdBy: g.relation(() => User),
+});
+
 export default config({
-  schema: g
+  schema: g,
   // Integrate Auth
   // https://grafbase.com/docs/auth
   // auth: {
@@ -29,4 +30,4 @@ export default config({
   //     rules.private()
   //   }
   // }
-})
+});
